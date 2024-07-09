b62c0c116e9eb4b23ada8a89f1a1c5d86296857c
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-07-09 12:16:00 +0700

    Initial commit

---
+++ lib/App/YoutubeDlUtils.pm
@@ -0,0 +1,31 @@
+package App::YoutubeDlUtils;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+use Log::ger;
+
+our %SPEC;
+
+$SPEC{':package'} = {
+    v => 1.1,
+    summary => 'Utilities (mostly wrappers) related to youtube-dl',
+};
+
+1;
+# ABSTRACT:
+
+=head1 SEE ALSO
+
+youtube-dl, L<https://youtube-dl.org>
+
+L<App::YouTubeDlIf>
+
+L<App::YouTubeUtils>
+
+=cut
